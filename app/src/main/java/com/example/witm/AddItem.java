b33a7e058998b1e64a48169268126b8947be997e
thuhaphamdlb <49466560+thuhaphamdlb@users.noreply.github.com>
2019-10-17 15:24:21 +0700

    add

--- app/src/main/java/com/example/witm/AddItem.java
+++ app/src/main/java/com/example/witm/AddItem.java
@@ -2,9 +2,7 @@ package com.example.witm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class AddItem extends AppCompatActivity {
 
