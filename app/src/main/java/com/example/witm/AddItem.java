3069f5984decb9b93796d48f82f4e8aaaa20ea42
Pham Thi Thu Ha <ha.pham@pn.com.vn>
2019-10-18 16:44:10 +0700

    ano

--- app/src/main/java/com/example/witm/AddItem.java
+++ app/src/main/java/com/example/witm/AddItem.java
@@ -2,13 +2,19 @@ package com.example.witm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AddItem extends AppCompatActivity {
-
+    TextView tvUserName = (TextView) findViewById(R.id.tvUserName);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        final Intent infor = this.getIntent();
+        String userName = infor.getStringExtra("email");
+        tvUserName.setText(":" + userName);
     }
 }
--- app/src/main/java/com/example/witm/CreateAccount.java
+++ app/src/main/java/com/example/witm/CreateAccount.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateAccount extends AppCompatActivity {
-
+    final EditText edtUserName = (EditText) findViewById(R.id.edtUserName);
+    Button button_login = (Button) findViewById(R.id.btnUserName);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,8 @@ public class CreateAccount extends AppCompatActivity {
             public void onClick(View view) {
                 Intent myIntent = new Intent(view.getContext(), AddItem.class);
                 startActivityForResult(myIntent, 0);
+                String email = edtUserName.getText().toString();
+                startActivityForResult(myIntent,0);
             }
         });
     }
